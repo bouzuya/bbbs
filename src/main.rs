01ffc8c9ea2fe382652e5a6cdfeec8b393f4d254
bouzuya <m@bouzuya.net>
2025-06-05 22:44:14 +0900

    Add port option

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,21 @@ impl Name for AppState {
     }
 }
 
+#[derive(clap::Parser)]
+struct Cli {
+    #[clap(long)]
+    port: Option<u16>,
+}
+
 #[tokio::main]
 async fn main() {
+    let cli = <Cli as clap::Parser>::parse();
+    let port = cli.port.unwrap_or(3000);
+
     let router = router().with_state(AppState {
         name: "bouzuya".to_owned(),
     });
-    let listener = tokio::net::TcpListener::bind(("0.0.0.0", 3000))
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
         .await
         .unwrap();
     axum::serve(listener, router.into_make_service())
