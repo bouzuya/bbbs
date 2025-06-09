02e470e2dedf8a3d73b33bff2c5d52a48e356492
bouzuya <m@bouzuya.net>
2025-06-10 06:54:43 +0900

    Rename MessageEvent to ThreadEvent and add Replied

--- src/model/shared/event.rs
+++ src/model/shared/event.rs
@@ -1,15 +1,27 @@
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 #[serde(tag = "kind", rename_all = "snake_case")]
-pub enum MessageEvent {
-    Created(MessageCreated),
+pub enum ThreadEvent {
+    Created(ThreadCreated),
+    Replied(ThreadReplied),
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
-pub struct MessageCreated {
+pub struct ThreadCreated {
     pub at: String,
     pub content: String,
     pub id: String,
     pub message_id: String,
+    pub thread_id: String,
+    pub version: i64,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
+pub struct ThreadReplied {
+    pub at: String,
+    pub content: String,
+    pub id: String,
+    pub message_id: String,
+    pub thread_id: String,
     pub version: i64,
 }
 
@@ -19,24 +31,65 @@ mod tests {
 
     #[test]
     fn test_message_event_created() -> anyhow::Result<()> {
+        let at = "2023-10-01T12:00:00.000Z".to_owned();
+        let content = "Hello, world!".to_owned();
+        let id = "0779b098-f41d-404a-b055-36463a7c009b".to_owned();
+        let message_id = "524313c5-6810-4f5d-a2a1-66a2224c74dc".to_owned();
+        let thread_id = "b8392399-53a3-4f8e-8288-875448037455".to_owned();
+        let version = 1;
         assert_eq!(
-            serde_json::from_str::<MessageEvent>(
+            serde_json::from_str::<ThreadEvent>(&format!(
                 r#"
-{
-    "at": "2023-10-01T12:00:00.000Z",
-    "content": "Hello, world!",
-    "id": "0779b098-f41d-404a-b055-36463a7c009b",
+{{
+    "at": "{at}",
+    "content": "{content}",
+    "id": "{id}",
     "kind": "created",
-    "message_id": "524313c5-6810-4f5d-a2a1-66a2224c74dc",
-    "version": 1
-}"#,
-            )?,
-            MessageEvent::Created(MessageCreated {
-                at: "2023-10-01T12:00:00.000Z".to_owned(),
-                content: "Hello, world!".to_owned(),
-                id: "0779b098-f41d-404a-b055-36463a7c009b".to_owned(),
-                message_id: "524313c5-6810-4f5d-a2a1-66a2224c74dc".to_owned(),
-                version: 1,
+    "message_id": "{message_id}",
+    "thread_id": "{thread_id}",
+    "version": {version}
+}}"#
+            ))?,
+            ThreadEvent::Created(ThreadCreated {
+                at,
+                content,
+                id,
+                message_id,
+                thread_id,
+                version,
+            })
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_message_event_replied() -> anyhow::Result<()> {
+        let at = "2023-10-01T12:00:00.000Z".to_owned();
+        let content = "Reply to message".to_owned();
+        let id = "0779b098-f41d-404a-b055-36463a7c009b".to_owned();
+        let message_id = "524313c5-6810-4f5d-a2a1-66a2224c74dc".to_owned();
+        let thread_id = "b8392399-53a3-4f8e-8288-875448037455".to_owned();
+        let version = 2;
+        assert_eq!(
+            serde_json::from_str::<ThreadEvent>(&format!(
+                r#"
+{{
+    "at": "{at}",
+    "content": "{content}",
+    "id": "{id}",
+    "kind": "replied",
+    "message_id": "{message_id}",
+    "thread_id": "{thread_id}",
+    "version": {version}
+}}"#
+            ))?,
+            ThreadEvent::Replied(ThreadReplied {
+                at,
+                content,
+                id,
+                message_id,
+                thread_id,
+                version,
             })
         );
         Ok(())
