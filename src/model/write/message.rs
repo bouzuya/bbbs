2ef90cdd318ded9ffbb2470ccf82cfc1e17dbaa5
bouzuya <m@bouzuya.net>
2025-06-09 19:16:42 +0900

    Add Message::new_for_testing

--- src/model/write/message.rs
+++ src/model/write/message.rs
@@ -2,7 +2,7 @@ use crate::model::shared::id::MessageId;
 use crate::model::write::MessageContent;
 use crate::utils::date_time::DateTime;
 
-#[derive(Clone)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Message {
     pub content: MessageContent,
     pub created_at: DateTime,
@@ -17,6 +17,15 @@ impl Message {
             id: MessageId::generate(),
         }
     }
+
+    #[cfg(test)]
+    pub fn new_for_testing() -> Self {
+        Self {
+            content: MessageContent::new_for_testing(),
+            created_at: DateTime::now(),
+            id: MessageId::generate(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -24,10 +33,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_message_create() {
+    fn test_create() {
         let content = MessageContent::new_for_testing();
         let message = Message::create(content.clone());
         assert_eq!(message.content, content);
         assert!(!message.id.to_string().is_empty());
+    }
+
+    #[test]
+    fn test_new_for_testing() {
+        let message1 = Message::new_for_testing();
+        let message2 = Message::new_for_testing();
+        assert_ne!(message1, message2);
     }
 }
