b4392218f755852588a4d9cd9b88f6c4208a7949
bouzuya <m@bouzuya.net>
2025-06-09 19:13:25 +0900

    Add methods to version

--- src/model/write/version.rs
+++ src/model/write/version.rs
@@ -1,2 +1,13 @@
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Version(u32);
+
+impl Version {
+    pub fn initial() -> Self {
+        Self(1)
+    }
+
+    pub fn next(&self) -> Self {
+        // TODO: overflow
+        Self(self.0 + 1)
+    }
+}
