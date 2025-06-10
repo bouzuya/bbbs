40afd35aea117242ce1bc5b5951803917bb479d4
bouzuya <m@bouzuya.net>
2025-06-10 21:08:34 +0900

    Use thread

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,31 @@ mod utils;
 
 use std::sync::{Arc, Mutex};
 
+struct Store {
+    read: crate::model::read::Thread,
+    write: crate::model::write::Thread,
+}
+
 #[derive(Clone)]
 struct AppState {
-    messages: Arc<Mutex<Vec<crate::model::read::Message>>>,
+    store: Arc<Mutex<Store>>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        let (thread, events) =
+            crate::model::write::Thread::create(crate::model::write::Message::create(
+                crate::model::write::MessageContent::try_from("_".to_owned())
+                    .expect("dummy message content to be valid"),
+            ))
+            .expect("dummy thread creation to be successful");
+        AppState {
+            store: Arc::new(Mutex::new(Store {
+                read: crate::model::read::Thread::replay(events),
+                write: thread,
+            })),
+        }
+    }
 }
 
 impl crate::handler::messages::MessageReader for AppState {
@@ -14,14 +36,14 @@ impl crate::handler::messages::MessageReader for AppState {
         &self,
         id: &crate::model::shared::id::MessageId,
     ) -> Option<crate::model::read::Message> {
-        let messages = self.messages.lock().unwrap();
+        let store = self.store.lock().unwrap();
         let s = id.to_string();
-        messages.iter().find(|it| &it.id == &s).cloned()
+        store.read.messages.iter().find(|it| &it.id == &s).cloned()
     }
 
     fn list_messages(&self) -> Vec<crate::model::read::Message> {
-        let messages = self.messages.lock().unwrap();
-        messages.clone()
+        let store = self.store.lock().unwrap();
+        store.read.messages.clone()
     }
 }
 
@@ -29,10 +51,18 @@ impl crate::handler::messages::MessageRepository for AppState {
     fn store(
         &self,
         _version: Option<crate::model::write::Version>,
+        // TODO: use event
         message: &crate::model::write::Message,
     ) -> Result<(), handler::messages::MessageRepositoryError> {
-        let mut messages = self.messages.lock().unwrap();
-        messages.push(crate::model::read::Message {
+        let mut store = self.store.lock().unwrap();
+        // TODO: add event
+        store
+            .write
+            .reply(message.clone())
+            .map_err(Into::into)
+            .map_err(handler::messages::MessageRepositoryError::InternalError)?;
+        // TODO: use apply
+        store.read.messages.push(crate::model::read::Message {
             content: String::from(message.content.clone()),
             created_at: message.created_at.to_string(),
             id: message.id.to_string(),
@@ -52,9 +82,7 @@ async fn main() {
     let cli = <Cli as clap::Parser>::parse();
     let port = cli.port.unwrap_or(3000);
 
-    let router = handler::router().with_state(AppState {
-        messages: Arc::new(Mutex::new(vec![])),
-    });
+    let router = handler::router().with_state(AppState::new());
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
         .await
         .unwrap();
--- src/model/write.rs
+++ src/model/write.rs
@@ -5,4 +5,5 @@ mod version;
 
 pub use self::message::Message;
 pub use self::message_content::{MessageContent, MessageContentError};
+pub use self::thread::{Thread, ThreadError};
 pub use self::version::Version;
