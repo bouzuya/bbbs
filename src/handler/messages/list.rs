87c322d539dd266940461ab9e456181897ee78c1
bouzuya <m@bouzuya.net>
2025-06-11 22:32:11 +0900

    Remove unused response field

--- src/handler/messages/list.rs
+++ src/handler/messages/list.rs
@@ -6,7 +6,6 @@ use crate::handler::messages::MessageReader;
 #[derive(askama::Template)]
 #[template(path = "messages/index.html")]
 pub struct MessageListResponse {
-    pub thread_id: String,
     pub messages: Vec<crate::model::read::Message>,
 }
 
@@ -20,8 +19,6 @@ impl axum::response::IntoResponse for MessageListResponse {
 
 pub async fn handler<S: MessageReader>(State(state): State<S>) -> MessageListResponse {
     MessageListResponse {
-        // TODO: dummy thread id
-        thread_id: "5868d08d-12d7-468f-b77e-cb6e837baaf9".to_owned(),
         messages: state.list_messages(),
     }
 }
