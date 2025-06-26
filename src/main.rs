0c3a81fbbcd345b342e297da87c7786e9fc2a155
bouzuya <m@bouzuya.net>
2025-06-26 21:59:14 +0900

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod port;
 mod store;
 mod utils;
 
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 use crate::app_state::AppState;
 
 #[derive(clap::Parser)]
@@ -15,10 +17,29 @@ struct Cli {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     let cli = <Cli as clap::Parser>::parse();
     let port = cli.port.unwrap_or(3000);
 
-    let router = handler::router().with_state(AppState::new());
+    let router = handler::router().with_state(AppState::new()).layer(
+        tower_http::trace::TraceLayer::new_for_http().make_span_with(
+            |request: &axum::http::Request<axum::body::Body>| {
+                let matched_path = request
+                    .extensions()
+                    .get::<axum::extract::MatchedPath>()
+                    .map(axum::extract::MatchedPath::as_str);
+                tracing::info_span!(
+                    "http_request",
+                    matched_path,
+                    method = ?request.method(),
+                    uri = ?request.uri(),
+                )
+            },
+        ),
+    );
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
         .await
         .unwrap();
