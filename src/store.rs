df616f43235cd1f01952990d859a80599eb40189
bouzuya <m@bouzuya.net>
2025-07-04 23:27:05 +0900

    Add allow(unused_imports)

--- src/store.rs
+++ src/store.rs
@@ -2,7 +2,9 @@ mod in_memory_store;
 #[cfg(feature = "sqlite")]
 mod sqlite_store;
 
+#[allow(unused_imports)]
 pub use self::in_memory_store::InMemoryStore;
+#[allow(unused_imports)]
 #[cfg(feature = "sqlite")]
 pub use self::sqlite_store::SqliteStore;
 
