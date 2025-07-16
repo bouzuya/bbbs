45bded1b834aad81c44ff0fa3ec45111a86e37b8
bouzuya <m@bouzuya.net>
2025-07-16 22:23:13 +0900

    Add firestore_store

--- src/store.rs
+++ src/store.rs
@@ -1,7 +1,12 @@
+#[cfg(feature = "firestore")]
+mod firestore_store;
 mod in_memory_store;
 #[cfg(feature = "sqlite")]
 mod sqlite_store;
 
+#[allow(unused_imports)]
+#[cfg(feature = "firestore")]
+pub use self::firestore_store::FirestoreStore;
 #[allow(unused_imports)]
 pub use self::in_memory_store::InMemoryStore;
 #[allow(unused_imports)]
